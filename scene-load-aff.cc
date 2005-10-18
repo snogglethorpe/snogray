b1b1f69ef4dfb27cab2bfe0fb0056ce2846a52ee
Miles Bader <miles@gnu.org>
2005-10-18 10:49:38 +0000

    Set camera z_mode to Z_DECREASES_FORWARD when loading .nff/.aff file
    git-archimport-id: miles@gnu.org--gnu-2005/snogray--devo--0--patch-74

--- scene-load-aff.cc
+++ scene-load-aff.cc
@@ -246,6 +246,7 @@ Scene::load_aff_file (istream &stream, Camera &camera)
   const Material *cur_material = 0;
 
   set_assumed_gamma (AFF_ASSUMED_GAMMA);
+  camera.set_z_mode (Camera::Z_DECREASES_FORWARD);
 
   while (! stream.eof ())
     {
