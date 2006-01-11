340e89de3a3af32d63d5c33785a15b60f7719e2b
Miles Bader <miles@gnu.org>
2006-01-11 08:09:58 +0000

    Tweak test-scene listing option output
    git-archimport-id: miles@gnu.org--gnu-2005/snogray--devo--0--patch-157

--- snogray.cc
+++ snogray.cc
@@ -1,6 +1,6 @@
 // snogray.cc -- Main driver for snogray ray tracer
 //
-//  Copyright (C) 2005  Miles Bader <miles@gnu.org>
+//  Copyright (C) 2005, 2006  Miles Bader <miles@gnu.org>
 //
 // This file is subject to the terms and conditions of the GNU General
 // Public License.  See the file COPYING in the main directory of this
@@ -370,8 +370,8 @@ int main (int argc, char *const *argv)
 	       di != descs.end(); di++)
 	    {
 	      unsigned nlen = 15;
-	      while (di->name.length() > nlen)
-		nlen += 10;
+	      while (di->name.length() > nlen - 3)
+		nlen += 12;
 	      cout << "   " << setw(nlen) << di->name << di->desc << endl;
 	    }
 	}
