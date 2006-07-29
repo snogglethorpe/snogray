e33588185feaae56dc2bdafaab6cc91de7dbaa52
Miles Bader <miles@gnu.org>
2006-07-29 02:40:14 +0000

    Implement support for reading "old format" radiance RGBE files
    git-archimport-id: miles@gnu.org--2006/snogray--devo--0--patch-191

--- image-rgbe.cc
+++ image-rgbe.cc
@@ -19,10 +19,20 @@
 // The ASCII size-indicator line is of the format:  -Y height +X width
 // where "height" and "width" are integers.
 //
-// Each scanline consists of two constant bytes with a value 2, a 16-bit
-// big-endian line-length, and four run-length-encoded sections, containing
-// all the red bytes for the scanline, then all the blue bytes, all the
-// green bytes, then all the exponent bytes.
+// There are two different scanline formats:
+// 
+// * In "new" format, each scanline consists of two constant bytes with
+//   the value 2, a 16-bit big-endian line-length, and four
+//   run-length-encoded sections, containing all the red bytes for the
+//   scanline, then all the blue bytes, all the green bytes, then all
+//   the exponent bytes.
+//
+// * In "old" format, each scanline consists of (R,G,B,EXP) tuples;
+//   special "repeat tuples" with R=G=B=1 store a repeat count in the EXP
+//   byte, and cause the previous color to be repeated that many times.
+//   If more than one repeat tuple occurs in a row, each subsequent
+//   repeat tuple has its repeat count multiplied by 256 more than the
+//   previous repeat tuple.
 //
 // The run-length-encoding consists of a series of runs, each run containing
 // a length byte, LEN, and either a single data byte (if LEN is greater than
@@ -222,10 +232,15 @@ RgbeImageSource::read_rle_component (byte RgbeColor::*component)
 void
 RgbeImageSource::read_row (ImageRow &row)
 {
-  // Each line begins with two constant bytes, which check
+  // We try to distinguish between "old" or "new" formats, by examining
+  // the first few bytes, and seeing if they look like a valid "new"
+  // format line header.
+
+  // The first two bytes should be have the constant value 2 in "new"
+  // format.
   //
-  if (inf.get() != 2 || inf.get() != 2)
-    err ("invalid line header");
+  unsigned lh0 = inf.get ();
+  unsigned lh1 = inf.get ();
 
   // The next two bytes are the length of the line in pixels encoded as
   // a big-endian 16-bit number.  This should match the width of the
@@ -233,16 +248,76 @@ RgbeImageSource::read_row (ImageRow &row)
   //
   unsigned ll_hi = inf.get ();
   unsigned ll_lo = inf.get ();
-  if ((ll_hi << 8) + ll_lo != width)
-    err ("line/image width mismatch");
 
-  // Read in the various components of the rgbe-encoded colors.
-  //
-  read_rle_component (&RgbeColor::r);
-  read_rle_component (&RgbeColor::g);
-  read_rle_component (&RgbeColor::b);
-  read_rle_component (&RgbeColor::exp);
+  if (lh0 == 2 && lh1 == 2 && (ll_hi << 8) + ll_lo == width)
+    //
+    // Looks like "new" format
+    {
+      // Read in the various components of the rgbe-encoded colors.
+      //
+      read_rle_component (&RgbeColor::r);
+      read_rle_component (&RgbeColor::g);
+      read_rle_component (&RgbeColor::b);
+      read_rle_component (&RgbeColor::exp);
+    }
+  else
+    //
+    // Didn't make any sense in "new" format, must be "old" format.
+    {
+      // Interpret the apparently bogus line header as the first pixel.
+      //
+      row_buf[0].r = lh0;
+      row_buf[0].g = lh1;
+      row_buf[0].b = ll_hi;
+      row_buf[0].exp = ll_lo;
 
+      // Pixel index; we already have pixel 0.
+      //
+      unsigned pix = 1;
+
+      // "repeat-count shift" -- 8-bit repeat counts are left-shifted by
+      // this amount.  Consecutive magic repeat-count pixels bump this
+      // by 8 each time, allowing very large repeat counts to be
+      // specified.
+      //
+      unsigned rep_shift = 0;
+
+      while (pix < width)
+	{
+	  // Read next pixel.
+	  //
+	  row_buf[pix].r = inf.get ();
+	  row_buf[pix].g = inf.get ();
+	  row_buf[pix].b = inf.get ();
+	  row_buf[pix].exp = inf.get ();
+
+	  // See if the pixel "color" is really a repeat count for
+	  // previous pixel.
+	  //
+	  if (row_buf[pix].r == 1 && row_buf[pix].g == 1 && row_buf[pix].b == 1)
+	    //
+	    // Yes
+	    {
+	      unsigned rep_count = row_buf[pix].exp << rep_shift;
+
+	      if (rep_count > width - pix)
+		err ("pixel repeat-count too large in old-format RGBE line");
+
+	      while (rep_count-- != 0)
+		row_buf[pix] = row_buf[pix - 1];
+
+	      rep_shift += 8;	// make next repeat count larger
+	    }
+	  else
+	    //
+	    // No, just a normal color.
+	    {
+	      pix++;
+	      rep_shift = 0;
+	    }
+	}
+    }
+    
   // Transform them to our internal color format.
   //
   for (unsigned i = 0; i < width; i++)
