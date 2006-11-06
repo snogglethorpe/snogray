e32b257912cb0b3e90600f93270024a9d7ff83d6
Miles Bader <miles@gnu.org>
2006-11-06 09:27:21 +0000

    Try to avoid returning 1.f from random(float)
    git-archimport-id: miles@gnu.org--2006/snogray--devo--0--patch-291

--- rand.h
+++ rand.h
@@ -16,25 +16,52 @@
 
 namespace Snogray {
 
+// Double-precision random numbers
+
+// Multiplier to turn the integer result from rand(), with range
+// [0, MAX_RAND], into a double, range [0, 1).
+//
+static const double drand_scale = 1 / (double (RAND_MAX) + 1);
+
+inline double drand () { return double (rand()) * drand_scale; }
+
 inline double random (double limit = 1.0)
 {
-  return double (rand()) * limit / double (RAND_MAX);
+  return drand() * limit;
 }
 
 inline double random (double min, double limit)
 {
-  return (double (rand()) * (limit - min) / double (RAND_MAX)) + min;
+  return (drand() * (limit - min)) + min;
 }
+
+
+// Single-precision random numbers
+
+// Single-precision random numbers are a bit harder, as RAND_MAX
+// (on a 32-bit machine) won't fit in a float, and just using
+// double for the calculation and converting to float will
+// sometimes end up rounding to 1.  So we just fiddle around to
+// avoid doing so.
+
+#define RAND_MAX_BITS 		31 // XXX CONFIGURE THIS!!
+#define FLOAT_MANTISSA_BITS	24 // XXX ...likewise
+#define FRAND_SHIFT		(RAND_MAX_BITS - FLOAT_MANTISSA_BITS)
+
+static const float frand_scale = 1 / float ((RAND_MAX >> FRAND_SHIFT) + 1);
+
+inline float frand () { return float (rand() >> FRAND_SHIFT) * frand_scale; }
 
 inline float random (float limit = 1.0)
 {
-  return float (rand()) * limit / float (RAND_MAX);
+  return frand() * limit;
 }
 
 inline float random (float min, float limit)
 {
-  return (float (rand()) * (limit - min) / float (RAND_MAX)) + min;
+  return (frand() * (limit - min)) + min;
 }
+
 
 }
 
