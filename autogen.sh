7db89f3272e7bfcd1b009345104232db89ee19b2
Miles Bader <miles@gnu.org>
2006-12-22 02:57:24 +0000

    Use "autoreconf" in autogen.sh
    git-archimport-id: miles@gnu.org--2006/snogray--devo--0--patch-320

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-aclocal			&&
-automake -a --foreign	&&
-autoconf
+AUTOMAKE="${AUTOMAKE:-automake} --foreign --add-missing" autoreconf
 
 # arch-tag: 3abe33d2-2b3e-4aa4-8d0f-2c64f6d19498
