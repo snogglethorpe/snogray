edf34d42b0f094b3b362c4c033bb52afe432cf36
Miles Bader <miles@gnu.org>
2007-05-29 02:17:17 +0000

    autogen.sh: Pass through user args to autoreconf
    git-archimport-id: miles@gnu.org/snogray--devo--0--patch-7

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-AUTOMAKE="${AUTOMAKE:-automake} --foreign --add-missing" autoreconf
+AUTOMAKE="${AUTOMAKE:-automake} --foreign --add-missing" autoreconf "$@"
 
 # arch-tag: 3abe33d2-2b3e-4aa4-8d0f-2c64f6d19498
