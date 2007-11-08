b8953b3a6aa080843364263e1e5ca3ab47aa2913
Miles Bader <miles@gnu.org>
2007-11-08 14:10:29 +0900

    Rewrite load-obj.lua to load the entire file at once
    Doing so is slightly faster than operating line-by-line, and is a better
    basis to build on for other file formats.

--- load-obj.lua
+++ load-obj.lua
@@ -3,7 +3,7 @@
 local lpeg = require 'lpeg'
 
 -- local abbreviations for lpegp primitives
-local P, R, S = lpeg.P, lpeg.R, lpeg.S
+local P, R, S, C = lpeg.P, lpeg.R, lpeg.S, lpeg.C
 
 -- number components
 local p_pm = S"+-"^-1
@@ -19,17 +19,42 @@ local p_float = (p_fmant * p_fexp^-1) / tonumber
 -- whitespace
 local p_opt_ws = S" \t\r\n\f"^0	-- optional whitespace
 local p_req_ws = S" \t\r\n\f"^1 -- required whitespace
+local p_opt_horiz_ws = S" \t"^0	-- optional non-newline whitespace
+local p_req_horiz_ws = S" \t"^1 -- required non-newline whitespace
 local p_nl = S"\r\n\f"		-- newline char
+
+-- line
+local p_line = (1 - p_nl)^0
+local p_line_nl = p_line * p_nl
+local p_line_contents = C (p_line)
 
 -- whitespace followed by numbers (useful shortcuts)
 local p_ws_float = p_opt_ws * p_float
 local p_ws_int = p_opt_ws * p_int
 
 -- obj-file comment
-local p_comment = P"#" * (1 - p_nl)^0
+local p_comment = P"#" * p_line
+
+
+-- Signals an error with a simple message quoting the problem line
+--
+function parse_err (text, pos)
+   local line_num = 1
+   local count_pos = 1
+   while count_pos < pos do
+      line_num = line_num + 1
+      print ("count_pos",count_pos)
+      count_pos = p_line_nl:match (text, count_pos)
+   end
+   error ("parse error at position "..tostring(line_num)..": "
+	  ..p_line_contents:match (text, pos))
+end
 
 function load_obj (filename, mesh, mat_map)
-   local s = io.open (filename, "r")
+   local stream, err = io.open (filename, "r")
+   if not stream then
+      error (err)
+   end
 
    local mat = mat_map:get_default ()
 
@@ -60,12 +85,19 @@ function load_obj (filename, mesh, mat_map)
 
    local p_v_cmd = P"v" * ((p_ws_float * p_ws_float * p_ws_float) / add_vert)
    local p_f_cmd = P"f" * (p_ws_int^2 / add_poly)
-   local p_cmd = p_v_cmd + p_f_cmd + p_comment + p_opt_ws
+   local p_cmd = p_v_cmd + p_f_cmd + p_comment + p_opt_horiz_ws
+   local p_cmd_line = p_cmd * p_nl
 
-   for line in s:lines () do
-      if not p_cmd:match (line) then
-	 error ("invalid line in obj file \""..line.."\"")
+   local text = stream:read'*a'
+
+   local len = #text
+   local pos = 1
+   while pos <= len do
+      local next_pos = p_cmd_line:match (text, pos)
+      if not next_pos then
+	 parse_err (text, pos)
       end
+      pos = next_pos
    end
 
    return true
