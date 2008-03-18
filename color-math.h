5aa1423b9f2449084c16e88b59ce6826dba0021d
Miles Bader <miles@gnu.org>
2008-03-18 09:24:00 +0900

    Add color-math.h
    It contains various standard math functions overloaded to work on
    colors.

---
+++ color-math.h
@@ -0,0 +1,57 @@
+// color-math.h -- Various standard math functions overloaded to work on colors
+//
+//  Copyright (C) 2008  Miles Bader <miles@gnu.org>
+//
+// This source code is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3, or (at
+// your option) any later version.  See the file COPYING for more details.
+//
+// Written by Miles Bader <miles@gnu.org>
+//
+
+#ifndef __COLOR_MATH_H__
+#define __COLOR_MATH_H__
+
+#include "snogmath.h"
+#include "color.h"
+
+
+namespace snogray {
+
+
+#define DEF_COLOR_MATH_FUN1(fun)				\
+  inline Color fun (const Color &col)				\
+  {								\
+    Color rval;							\
+    for (unsigned c = 0; c < Color::NUM_COMPONENTS; c++)	\
+      rval[c] = fun (col[c]);					\
+    return rval;						\
+  }
+
+#define DEF_COLOR_MATH_FUN2(fun)				\
+  inline Color fun (const Color &col1, const Color &col2)	\
+  {								\
+    Color rval;							\
+    for (unsigned c = 0; c < Color::NUM_COMPONENTS; c++)	\
+      rval[c] = fun (col1[c], col2[c]);				\
+    return rval;						\
+  }
+
+
+DEF_COLOR_MATH_FUN1 (floor)
+DEF_COLOR_MATH_FUN1 (ceil)
+DEF_COLOR_MATH_FUN1 (trunc)
+DEF_COLOR_MATH_FUN1 (sin)
+DEF_COLOR_MATH_FUN1 (cos)
+DEF_COLOR_MATH_FUN1 (tan)
+
+DEF_COLOR_MATH_FUN2 (atan2)
+
+
+}
+
+#endif /* __COLOR_H__ */
+
+
+// arch-tag: 389b3ebb-55a4-4d70-afbe-91bdb72d28ed
