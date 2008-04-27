68c683e2f3dc0c9639af575e191bdca9c59c6586
Miles Bader <miles@gnu.org>
2008-04-28 08:18:25 +0900

    Add compiler.h

---
+++ compiler.h
@@ -0,0 +1,35 @@
+// compiler.h -- compiler-specific definitions
+//
+//  Copyright (C) 2008  Miles Bader <miles@gnu.org>
+//
+// This source code is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3, or (at
+// your option) any later version.  See the file COPYING for more details.
+//
+// Written by Miles Bader <miles@gnu.org>
+//
+
+#ifndef __COMPILER_H__
+#define __COMPILER_H__
+
+// unlikely(EXPR) returns EXPR (which is usually a boolean expression),
+// and as a side-effect tries to tell the compiler that the expected
+// value is zero (false).
+//
+#ifdef __GNUC__
+# define unlikely(expr) __builtin_expect (expr, 0)
+#else
+# define unlikely(expr) (expr)
+#endif
+
+// likely(EXPR) returns EXPR converted to a boolean value, and as a
+// side-effect tries to tell the compiler that the expected value is true.
+//
+#ifdef __GNUC__
+# define likely(expr) __builtin_expect (!!(expr), true)
+#else
+# define likely(expr) (!!(expr))
+#endif
+
+#endif // __COMPILER_H__
