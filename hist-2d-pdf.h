8186b8973899fffbd2a29facdb4a003d6e463be2
Miles Bader <miles@gnu.org>
2010-02-28 17:21:56 +0900

    Add Hist2d and Hist2dPdf classes
    Hist2d is a simple 2d histogram (of floats).
    
    Hist2Pdf allows sampling based on a Hist2d, with the resulting
    samples having a distribution proportional to the values in the
    histogram.

---
+++ hist-2d-pdf.h
@@ -0,0 +1,241 @@
+// hist-2d-pdf.h -- 2d histogram-based PDF
+//
+//  Copyright (C) 2010  Miles Bader <miles@gnu.org>
+//
+// This source code is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3, or (at
+// your option) any later version.  See the file COPYING for more details.
+//
+// Written by Miles Bader <miles@gnu.org>
+//
+
+#ifndef __HIST_2D_PDF_H__
+#define __HIST_2D_PDF_H__
+
+#include <vector>
+#include <algorithm>
+
+#include "hist-2d.h"
+
+
+namespace snogray {
+
+
+// A 2d PDF based on a 2d histogram.  This is useful for doing cheap
+// re-sampling based on an arbitrary 2d input set.
+//
+class Hist2dPdf
+{
+public:
+
+  // This constructor allocates the necessary memory, but won't be
+  // usable until a histogram has been specified using Hist2dPdf::calc.
+  //
+  Hist2dPdf (unsigned w, unsigned h)
+    : width (w), height (h), size (w*h),
+      column_width (1.f / width), row_height (1.f / height),
+      whole_row_cumulative_sums (height),
+      individual_row_cumulative_sums (size)
+  { }
+
+  // This constructor automatically copies the size from HIST, and
+  // calculates the PDF.  No references to HIST is kept.
+  //
+  Hist2dPdf (const Hist2d &hist)
+    : width (hist.width), height (hist.height), size (height * width),
+      column_width (1.f / width), row_height (1.f / height),
+      whole_row_cumulative_sums (height),
+      individual_row_cumulative_sums (size)
+  { calc (hist); }
+
+  // Calculate the PDF based from the histogram HIST.  HIST's size must
+  // be the same as the size this object was created with.  No reference
+  // to HIST is kept.
+  //
+  void calc (const Hist2d &hist)
+  {
+    // Note, the use of double-precision floats here is intentional --
+    // HDR images can cause precision problems if single-precision
+    // floats are used.
+
+    unsigned row_offs;
+
+    // Find sum of entire input array.
+    //
+    double bin_sum = 0;
+    row_offs = 0;
+    for (unsigned row = 0; row < height; row++)
+      {
+	for (unsigned col = 0; col < width; col++)
+	  bin_sum += hist.bins[row_offs + col];
+	row_offs += width;
+      }
+
+    // Find cumulative sums of entire rows, normalized to the range 0-1
+    // (so the last row will always have a value of 1, except in the
+    // degenerate case where all bins are zero).
+    //
+    double inv_bin_sum = (bin_sum == 0) ? 0 : 1 / bin_sum;
+    double normalized_sum = 0;
+    row_offs = 0;
+    for (unsigned row = 0; row < height; row++)
+      {
+	for (unsigned col = 0; col < width; col++)
+	  normalized_sum += hist.bins[row_offs + col] * inv_bin_sum;
+	whole_row_cumulative_sums[row] = normalized_sum;
+	row_offs += width;
+      }
+
+    // Find cumulative sums within each row, normalized to the range 0-1
+    // (so for each row, the last column within the row will always have
+    // value 1, except in the degenerate case where all bins are zero).
+    //
+    row_offs = 0;
+    for (unsigned row = 0; row < height; row++)
+      {
+	double row_sum = 0;
+	for (unsigned col = 0; col < width; col++)
+	  row_sum += hist.bins[row_offs + col];
+
+	double inv_row_sum = (row_sum == 0) ? 0 : 1 / row_sum;
+	double normalized_row_sum = 0;
+	for (unsigned col = 0; col < width; col++)
+	  {
+	    normalized_row_sum += hist.bins[row_offs + col] * inv_row_sum;
+	    individual_row_cumulative_sums[row_offs + col] = normalized_row_sum;
+	  }
+	row_offs += width;
+      }
+  }
+
+  // Return a sample of this PDF based on the random variables in PARAM.
+  // The value of the PDF at the sample location is returned in _VAL.
+  //
+  // The returned UV coordinates should have roughly the same
+  // distribution as the input data (limited by the granularity of the
+  // histogram).
+  //
+  UV sample (const UV &param, float &_val) const
+  {
+    unsigned col, row, row_offs;
+    sample (param, col, row, row_offs);
+
+    _val = val (col, row, row_offs);
+
+    return UV (col * column_width + fmod (param.u, column_width),
+	       row * row_height   + fmod (param.v, row_height));
+  }
+
+  // Return a sample of this PDF based on the random variables in PARAM.
+  //
+  // The returned UV coordinates should have roughly the same
+  // distribution as the input data (limited by the granularity of the
+  // histogram).
+  //
+  UV sample (const UV &param) const
+  {
+    unsigned col, row, row_offs;
+    sample (param, col, row, row_offs);
+
+    return UV (col * column_width + fmod (param.u, column_width),
+	       row * row_height   + fmod (param.v, row_height));
+  }
+
+  // Return the value of the PDF at location POS.
+  //
+  float val (const UV &pos) const
+  {
+    unsigned col = clamp (int (pos.u * width), 0, int (width) - 1);
+    unsigned row = clamp (int (pos.v * height), 0, int (height) - 1);
+
+    return val (col, row, row * width);
+  }
+
+  const unsigned width, height, size;
+  const float column_width, row_height;
+
+private:
+
+  template<typename Ptr>
+  unsigned find_pos_in_sorted_vec (float val, const Ptr &beg, unsigned len)
+    const
+  {
+#if 0
+    unsigned pos = 0;
+    while (val > beg[pos])
+      pos++;
+    return pos;
+#else
+    // XXX std::lower_bound is much faster than a linear search for
+    // large histograms, but slower for small ones; it'd be nice to
+    // parameterize this somehow...
+    //
+    return std::lower_bound (beg, beg + len, val) - beg;
+#endif
+  }
+
+  // Sample the histogram and return the bin coordinates in COL, ROW.
+  // The offset in INDIVIDUAL_ROW_CUMULATIVE_SUMS of the row is also
+  // returned in ROW_OFFSET.
+  //
+  void sample (const UV &param,
+	       unsigned &col, unsigned &row, unsigned &row_offs)
+    const
+  {
+    float u = min (param.u, 1.f), v = min (param.v, 1.f);
+
+    // look in y dir.
+    //
+    row = find_pos_in_sorted_vec (v, whole_row_cumulative_sums.begin(), height);
+
+    // XXX this multiply actually uses a lot of time; it's nicer to
+    // accmulate the row-offset while finding the right how, or maybe
+    // keep a vector of row offsets?
+    //
+    row_offs = row * width;
+
+    // look in x dir
+    //
+    col = find_pos_in_sorted_vec (
+	    u, individual_row_cumulative_sums.begin() + row_offs, width);
+  }
+
+  // Return the value of the PDF for points in the bin located at COL,
+  // ROW, where ROW is the raw offset in INDIVIDUAL_ROW_CUMULATIVE_SUMS
+  // of the row.
+  //
+  float val (unsigned col, unsigned row, unsigned row_offs) const
+  {
+    // Probability of choosing this row.  Since
+    // WHOLE_ROW_CUMULATIVE_SUMS contains a cumulative sum of row
+    // probabilities, this is just the difference of the found row's
+    // entry minus the previous row's entry.
+    //
+    float row_prob = whole_row_cumulative_sums[row];
+    if (row != 0)
+      row_prob -= whole_row_cumulative_sums[row - 1];
+
+    // Probability of choosing this bin.  Similarly to ROW_PROB, this is
+    // just the difference of the current and previous entries in
+    // INDIVIDUAL_ROW_CUMULATIVE_SUMS.
+    //
+    float bin_prob = individual_row_cumulative_sums[row_offs + col];
+    if (col != 0)
+      bin_prob -= individual_row_cumulative_sums[row_offs + col - 1];
+    bin_prob *= row_prob;
+
+    // PDF = prob. of choosing a bin / bin area.
+    //
+    return bin_prob * size;
+  }
+
+public:
+  std::vector<float> whole_row_cumulative_sums;
+  std::vector<float> individual_row_cumulative_sums;
+};
+
+
+}
+
+#endif // __HIST_2D_PDF_H__
---
+++ hist-2d.h
@@ -0,0 +1,81 @@
+// hist-2d.h -- 2d histogram
+//
+//  Copyright (C) 2010  Miles Bader <miles@gnu.org>
+//
+// This source code is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3, or (at
+// your option) any later version.  See the file COPYING for more details.
+//
+// Written by Miles Bader <miles@gnu.org>
+//
+
+#ifndef __HIST_2D_H__
+#define __HIST_2D_H__
+
+#include <cstring> // memset
+#include <vector>
+
+#include "snogmath.h"
+#include "uv.h"
+
+
+namespace snogray {
+
+
+// A 2d histogram.
+//
+class Hist2d
+{
+public:
+
+  Hist2d (unsigned w, unsigned h)
+    : width (w), height (h), size (w*h), bins (size, 0)
+  { }
+
+  // Reset all state, to prepare for new input data.
+  //
+  void clear ()
+  {
+#if 0
+    std::fill (bins.begin(), bins.end(), 0);
+#else
+    memset (bins.data(), 0, size * sizeof (bins.front ()));
+#endif
+  }
+
+  // Record an input sample with position POS and value VAL.
+  //
+  void add (const UV &pos, float val)
+  {
+    unsigned col = min (unsigned (pos.u * width), width-1);
+    unsigned row = min (unsigned (pos.v * height), height-1);
+    bins[col + row*width] += val;
+  }
+
+  // Record an input sample with position (COL, ROW) in integer bin
+  // coordinates, and value VAL.
+  //
+  void add (unsigned col, unsigned row, float val)
+  {
+    bins[col + row*width] += val;
+  }
+
+  float &operator() (unsigned col, unsigned row)
+  {
+    return bins[col + row*width];
+  }
+  const float &operator() (unsigned col, unsigned row) const
+  {
+    return bins[col + row*width];
+  }
+
+  const unsigned width, height, size;
+
+  std::vector<float> bins;
+};
+
+
+}
+
+#endif // __HIST_2D_H__
