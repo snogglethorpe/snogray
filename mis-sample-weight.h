5ba8634193853148f8b6f3f39909f8a06461baa2
Miles Bader <miles@gnu.org>
2010-03-05 20:23:21 +0900

    Include an extra 1/NUM_SAMPLES factor in mis_sample_weight
    In places where we scale by the result of mis_sample_weight, we
    basically always want to subsequently divide by NUM_SAMPLES (since
    we use mis_sample_weight when averaging samples).
    
    Doing so implicitly in mis_sample_weight seems very natural, is a
    bit more efficient (it avoids a divide), and allows callers to be a
    bit simpler.
    
    Note that this change does not affect any existing code, as
    mis_sample_weight is currently only called with NUM_SAMPLES == 1.

--- mis-sample-weight.h
+++ mis-sample-weight.h
@@ -17,14 +17,30 @@
 namespace snogray {
 
 
-// Return a weight for one sample in multiple importantance sampling
-// with two sample distributions.  PDF is the pdf for the sample being
-// weighted, and NUM_SAMPLES is the number of samples taken from its
-// distribution.  OTHER_PDF and NUM_OTHER_SAMPLES are the corresponding
-// values from the other distribution being combined with this one via
-// MIS.
+// Return a weight for one sample in multiple importance sampling
+// with two sample distributions.  PDF is the pdf for the sample
+// being weighted, and NUM_SAMPLES is the number of samples taken
+// from its distribution.  OTHER_PDF and NUM_OTHER_SAMPLES are the
+// corresponding values from the other distribution being combined
+// with this one via MIS.
 //
 // This function is also known as the "power heuristic".
+//
+// The weight is calculated by squaring the product of the PDF and
+// number of samples for each distribution; the actual value
+// returned is:
+//
+//   (TERM^2 / (TERM^2 + OTHER_TERM^2)) * (1 / NUM_SAMPLES)
+//
+// where TERM = PDF * NUM_SAMPLES,
+// and OTHER_TERM = OTHER_PDF * NUM_OTHER_SAMPLES.
+//
+// Note that the return value includes an additional 1/NUM_SAMPLES
+// factor compared to the usual definition of the power heuristic;
+// this is done because we almost always want to divide by the
+// number of samples taken (since we're basically averaging the
+// samples), and it's cheaper to calculate here than separately
+// dividing by the number of samples afterwards.
 //
 static float
 mis_sample_weight (float pdf, float num_samples,
@@ -33,6 +49,9 @@ mis_sample_weight (float pdf, float num_samples,
   // This weight is calculated using the "power heuristic", with an
   // exponent of 2.
 
+  // TERM and OTHER_TERM are just the PDFs weighted by the number of
+  // samples.
+  //
   float term = num_samples * pdf;
   float other_term = num_other_samples * other_pdf;
 
@@ -46,10 +65,15 @@ mis_sample_weight (float pdf, float num_samples,
 
   // Raise the terms to a power of 2.
   //
-  term *= term;
-  other_term *= other_term;
+  float term_2 = term * term;
+  float other_term_2 = other_term * other_term;
 
-  return term / (term + other_term);
+  // Return (TERM^2 / (TERM^2 + OTHER_TERM^2)) * (1 / NUM_SAMPLES).
+  //
+  // Since TERM = PDF * NUM_SAMPLES, we avoid a divide by actually
+  // calculating (TERM * PDF) / (TERM^2 + OTHER_TERM^2) instead.
+  //
+  return (term * pdf) / (term_2 + other_term_2);
 }
 
 
