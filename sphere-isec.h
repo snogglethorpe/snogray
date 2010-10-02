30035d6850bfcc29bddb4ebf518e647d2f1a11f6
Miles Bader <miles@gnu.org>
2010-10-02 14:49:11 +0900

    Use more stable sphere-intersection test
    The old one would sometimes suffer precision problems in extreme
    cases (e.g., getting very close to very large spheres).

--- sphere-isec.h
+++ sphere-isec.h
@@ -16,6 +16,7 @@
 #include "pos.h"
 #include "vec.h"
 #include "ray.h"
+#include "quadratic-roots.h"
 
 
 namespace snogray {
@@ -31,42 +32,32 @@ namespace snogray {
 // from RAY_ORIGIN.  Only intersections with a parameter distance of
 // MIN_T or greater are considered.
 //
-template<typename T>
-bool
-sphere_intersects (const TPos<T> &center, T radius,
-		   const TPos<T> &ray_origin, const TVec<T> &ray_dir,
-		   T min_t, T &t)
+static inline bool
+sphere_intersects (const Pos &center, dist_t radius,
+		   const Pos &ray_origin, const Vec &ray_dir,
+		   dist_t min_t, dist_t &t)
 {
-  TVec<T> ray_origin_offs = ray_origin - center;
-  T dir_diff = dot (ray_dir, ray_origin_offs);
-  T dir_dir = dot (ray_dir, ray_dir); // theoretically, exactly 1; in
-				// practice, not _quite_
-  T diff_diff = dot (ray_origin_offs, ray_origin_offs);
-  T determ
-    = ((dir_diff * dir_diff) - dir_dir * (diff_diff - (radius * radius)));
+  Vec ray_origin_offs = ray_origin - center;
 
-  if (determ >= 0)
+  // Coefficients of the quadratic equation we'll solve.
+  //
+  dist_t a = ray_dir.length_squared ();
+  dist_t b = 2 * dot (ray_dir, ray_origin_offs);
+  dist_t c = ray_origin_offs.length_squared () - radius * radius;
+
+  // Compute intersection points.
+  //
+  dist_t roots[2];
+  unsigned nroots = quadratic_roots (a, b, c, roots);
+  for (unsigned i = 0; i < nroots; i++)
     {
-      t = -dir_diff / dir_dir;
-
-      if (determ > 0)
-	{
-	  T determ_factor = sqrt (determ) / dir_dir;
-
-	  // See which of T-DETERM_FACTOR or T+DETERM_FACTOR is greater
-	  // than MIN_T.
-	  //
-	  t -= determ_factor;	// first try -DETERM_FACTOR
-	  if (t < min_t)
-	    t += determ_factor + determ_factor; // then try +DETERM_FACTOR
-	}
-
-      return t >= min_t;
+      t = roots[i];
+      if (t > min_t)
+	return true;
     }
 
   return false;
 }
-
 
 // Return true if a sphere centered at CENTER with radius RADIUS is
 // intersected by an infinite ray from RAY_ORIGIN in direction
@@ -77,27 +68,25 @@ sphere_intersects (const TPos<T> &center, T radius,
 // of multiples of RAY_DIR required to reach the intersection point
 // from RAY_ORIGIN.
 //
-template<typename T>
-bool
-sphere_intersects (const TPos<T> &center, T radius,
-		   const TPos<T> &ray_origin, const TVec<T> &ray_dir,
-		   T &t)
+static inline bool
+sphere_intersects (const Pos &center, dist_t radius,
+		   const Pos &ray_origin, const Vec &ray_dir,
+		   dist_t &t)
 {
-  return sphere_intersects (center, radius, ray_origin, ray_dir, T(0), t);
+  return sphere_intersects (center, radius, ray_origin, ray_dir, 0, t);
 }
 
 
 // Return true if a sphere centered at CENTER with radius RADIUS is
-// intersected by the ray RAY.
+// intersected by RAY.
 //
 // When an intersection occurs, the "parametric distance" of the
 // intersection is returned in the out-parameter T:  T is the number
-// of multiples of RAY_DIR required to reach the intersection point
-// from RAY_ORIGIN.
+// of multiples of RAY's dir field required to reach the intersection
+// point from RAY's origin.
 //
-template<typename T>
-bool
-sphere_intersects (const TPos<T> &center, T radius, const TRay<T> &ray, T &t)
+static inline bool
+sphere_intersects (const Pos &center, dist_t radius, const Ray &ray, dist_t &t)
 {
   if (sphere_intersects (center, radius, ray.origin, ray.dir, ray.t0, t))
     return t < ray.t1;
