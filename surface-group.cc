853debbfdc97a6385c7fbf5268d7bdd578830d59
Miles Bader <miles@gnu.org>
2011-08-31 21:24:53 +0900

    Add SurfaceGroup intersection methods
    These are not normally called during rendering -- instead, individual
    component surfaces of a SurfaceGroup are added to acceleration
    structures, and their intersection methods are called instead -- but
    having them will keep the SurfaceGroup class concrete when we make
    Surface an abstract class.
    
    It's also nice to have them just for completeness (a user might, for
    instance want to use them on a surface not yet added to any
    acceleration structure).

--- surface-group.cc
+++ surface-group.cc
@@ -1,6 +1,6 @@
 // surface-group.cc -- Group of surfaces
 //
-//  Copyright (C) 2007, 2008, 2010  Miles Bader <miles@gnu.org>
+//  Copyright (C) 2007, 2008, 2010, 2011  Miles Bader <miles@gnu.org>
 //
 // This source code is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
@@ -60,4 +60,78 @@ SurfaceGroup::add_lights (std::vector<Light *> &lights) const
   for (std::vector<const Surface *>::const_iterator si = surfaces.begin();
        si != surfaces.end(); ++si)
     (*si)->add_lights (lights);
+}
+
+
+
+// SurfaceGroup intersection methods
+//
+// These are not normally called during rendering -- instead,
+// individual component surfaces of a SurfaceGroup are added to
+// acceleration structures, and their intersection methods are called
+// instead -- but are provided for completeness, and to make the
+// SurfaceGroup class concrete (Surface is an abstract class).
+
+
+// If this surface intersects RAY, change RAY's maximum bound (Ray::t1) to
+// reflect the point of intersection, and return a Surface::IsecInfo object
+// describing the intersection (which should be allocated using
+// placement-new with CONTEXT); otherwise return zero.
+//
+const Surface::IsecInfo *
+SurfaceGroup::intersect (Ray &ray, RenderContext &context) const
+{
+  const Surface::IsecInfo *closest = 0;
+
+  for (std::vector<const Surface *>::const_iterator si = surfaces.begin();
+       si != surfaces.end(); ++si)
+    {
+      const Surface::IsecInfo *isec_info = (*si)->intersect (ray, context);
+      if (isec_info)
+	closest = isec_info;
+    }
+
+  return closest;
+}
+
+// Return true if this surface intersects RAY.
+//
+bool
+SurfaceGroup::intersects (const Ray &ray, RenderContext &context) const
+{
+  for (std::vector<const Surface *>::const_iterator si = surfaces.begin();
+       si != surfaces.end(); ++si)
+    if ((*si)->intersects (ray, context))
+      return true;
+
+  return false;
+}
+
+// Return true if this surface completely occludes RAY.  If it does
+// not completely occlude RAY, then return false, and multiply
+// TOTAL_TRANSMITTANCE by the transmittance of the surface in medium
+// MEDIUM.
+//
+// Note that this method does not try to handle non-trivial forms of
+// transparency/translucency (for instance, a "glass" material is
+// probably considered opaque because it changes light direction as
+// well as transmitting it).
+//
+// [This interface is slight awkward for reasons of speed --
+// returning and checking for a boolean value for common cases is
+// significantly faster than, for instance, a simple "transmittance"
+// method, which requires handling Color values for all cases.]
+//
+bool
+SurfaceGroup::occludes (const Ray &ray, const Medium &medium,
+			Color &total_transmittance,
+			RenderContext &context)
+  const
+{
+  for (std::vector<const Surface *>::const_iterator si = surfaces.begin();
+       si != surfaces.end(); ++si)
+    if ((*si)->occludes (ray, medium, total_transmittance, context))
+      return true;
+
+  return false;
 }
--- surface-group.h
+++ surface-group.h
@@ -1,6 +1,6 @@
 // surface-group.h -- Group of surfaces
 //
-//  Copyright (C) 2007, 2008, 2010  Miles Bader <miles@gnu.org>
+//  Copyright (C) 2007, 2008, 2010, 2011  Miles Bader <miles@gnu.org>
 //
 // This source code is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
@@ -34,6 +34,37 @@ public:
   // Add SURFACE to this group.
   //
   void add (const Surface *surface);
+
+  // If this surface intersects RAY, change RAY's maximum bound (Ray::t1)
+  // to reflect the point of intersection, and return a Surface::IsecInfo
+  // object describing the intersection (which should be allocated using
+  // placement-new with CONTEXT); otherwise return zero.
+  //
+  virtual const IsecInfo *intersect (Ray &ray, RenderContext &context) const;
+
+  // Return true if this surface intersects RAY.
+  //
+  virtual bool intersects (const Ray &ray, RenderContext &context) const;
+
+  // Return true if this surface completely occludes RAY.  If it does
+  // not completely occlude RAY, then return false, and multiply
+  // TOTAL_TRANSMITTANCE by the transmittance of the surface in medium
+  // MEDIUM.
+  //
+  // Note that this method does not try to handle non-trivial forms of
+  // transparency/translucency (for instance, a "glass" material is
+  // probably considered opaque because it changes light direction as
+  // well as transmitting it).
+  //
+  // [This interface is slight awkward for reasons of speed --
+  // returning and checking for a boolean value for common cases is
+  // significantly faster than, for instance, a simple "transmittance"
+  // method, which requires handling Color values for all cases.]
+  //
+  virtual bool occludes (const Ray &ray, const Medium &medium,
+			 Color &total_transmittance,
+			 RenderContext &context)
+    const;
 
   // Return a bounding box for this surface.
   //
