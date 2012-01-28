230a4c0371df4377163a0776259d0f8d54955ab7
Miles Bader <miles@gnu.org>
2012-01-28 17:33:25 +0900

    Fix z_axis_* functions
    They were never correct, but weren't used either.

--- spherical-coords.h
+++ spherical-coords.h
@@ -1,6 +1,6 @@
 // spherical-coords.h -- Mappings from spherical coordinates to/from vectors
 //
-//  Copyright (C) 2010, 2011  Miles Bader <miles@gnu.org>
+//  Copyright (C) 2010-2012  Miles Bader <miles@gnu.org>
 //
 // This source code is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
@@ -33,28 +33,30 @@
 //
 // z-axis coordinates:  defined on a sphere with its axis along the z-axis
 //
-//   latitude:   angle between the vector and the x-y plane;
-//		 range -PI - PI; -PI means Vec(0,0,-1), PI means Vec(0,0,1)
+//   latitude:   angle between the vector and the x-y plane, with a
+//		 range -PI/2 - PI/2:  -PI/2 means Vec(0,0,-1), PI/2
+//		 means Vec(0,0,1)
 //
-//   colatitude: angle between the vector and the positive z-axis;
-//		 range 0 - PI; 0 means Vec(0,0,1), PI means Vec(0,0,-1)
+//   colatitude: angle between the vector and the positive z-axis, with a
+//		 range 0 - PI:  0 means Vec(0,0,1), PI means Vec(0,0,-1)
 //
-//   longitude:  angle between projection of the vector in the x-y plane
-//   		 and the y-axis; range -PI - PI; -PI and PI both mean
-//   		 Vec(0,1,0), 0 means Vec(0,-1,0), and PI/2 means Vec(1,0,0)
+//   longitude:  angle between a projection of the vector in the x-y
+//               plane and the x-axis, with a range -PI - PI:  0 means
+//               Vec(1,0,0), -PI and PI both mean Vec(-1,0,0), PI/2
+//               means Vec(0,-1,0), and -PI/2 means Vec(0,1,0).
 //
 //
 // UV mappings:  Each of the above types of coordinates can also be
 // represented in "UV" form, with two coordinates packed into a UV
 // object, and scaled to the range 0-1.
 //
-//   UV-spherical:  longitude and colatitude mapped to the range 0-1 via
-//   		 the following formulas:  u = longitude / (2*PI) + 0.5,
-//   		 v = colatitude / PI
+//   UV-spherical: longitude and colatitude mapped to the range 0-1
+//   		   via the following formulas:  u = longitude / (2*PI)
+//   		   + 0.5, v = colatitude / PI
 //
-//   UV-latlong: longitude and colatitude mapped to the range 0-1 via
-//   		 the following formulas:  u = longitude / (2*PI) + 0.5,
-//   		 v = latitude / PI + 0.5
+//   UV-latlong:   longitude and latitude mapped to the range 0-1 via
+//   		   the following formulas:  u = longitude / (2*PI) +
+//   		   0.5, v = (latitude / (PI/2)) + 0.5
 
 
 #include "snogmath.h"
@@ -135,65 +137,83 @@ y_axis_latlong (const TVec<T> &vec)
 
 // z-axis-based vector to spherical-coordinate conversion functions
 
-// Return the latitude of VEC, where the axis of the spherical
-// coordinates is the z-axis.  This is defined as the angle between
-// the vector and the x-y plane, with a range of -PI - PI; -PI means
-// Vec(0,0,-1), PI means Vec(0,0,1).
+// Return the "latitude" of VEC, where the axis of the spherical
+// coordinates is the z-axis.
+//
+// This is defined as the angle between the vector and the x-y plane,
+// with a range of -PI/2 - PI/2; -PI/2 means Vec(0,0,-1), PI/2 means
+// Vec(0,0,1).
 //
 template<typename T>
-static inline T
+float
 z_axis_latitude (const TVec<T> &vec)
 {
-  return atan2 (vec.z, sqrt (vec.x * vec.x + vec.y * vec.y));
+  // This is equiv to: atan2 (vec.z, sqrt (vec.x * vec.x + vec.y * vec.y))
+  // but asin is often faster than atan2 (and the division is negligible).
+  return
+    float (asin (clamp (vec.z
+			/ sqrt (vec.x * vec.x
+				+ vec.y * vec.y
+				+ vec.z * vec.z),
+			-1, 1)));
 }
 
-// Return the colatitude of VEC, where the axis of the spherical
-// coordinates is the z-axis.  This is defined as the angle between
-// the vector and the positive z-axis, with a range of 0 - PI; 0
-// means Vec(0,0,1), PI means Vec(0,0,-1).
+// Return the "colatitude" of VEC, where the axis of the spherical
+// coordinates is the z-axis.
+//
+// This is defined as the angle between the vector and the positive
+// z-axis, with a range of 0 - PI; 0 means Vec(0,0,1), PI means
+// Vec(0,0,-1).
 //
 template<typename T>
-static inline T
+float
 z_axis_colatitude (const TVec<T> &vec)
 {
-  return atan2 (sqrt (vec.x * vec.x + vec.y * vec.y), vec.z);
+  float lat = z_axis_latitude (vec);
+  return (PIf / 2) - lat;
 }
 
-// Return the longitude of VEC, where the axis of the spherical
-// coordinates is the z-axis.  This is defined as the angle between
-// a projection of the vector in the x-y plane and the y-axis, with
-// a range -PI - PI; -PI and PI both mean Vec(0,1,0), 0 means
-// Vec(0,-1,0), and PI/2 means Vec(1,0,0).
+// Return the "longitude" of VEC, where the axis of the spherical
+// coordinates is the z-axis.
+//
+// This is defined as the angle between a projection of the vector in
+// the x-y plane and the x-axis, with a range -PI - PI:  0 means
+// Vec(1,0,0), -PI and PI both mean Vec(-1,0,0), PI/2 means
+// Vec(0,-1,0), and -PI/2 means Vec(0,1,0).
 //
 template<typename T>
-static inline T
+float
 z_axis_longitude (const TVec<T> &vec)
 {
-  return atan2 (vec.x, vec.y);
+  return float (atan2 (-vec.y, vec.x));
 }
 
 // Return UV-encoded z-axis-based spherical coordinates for VEC.
+//
 // In the return value, u will be the longitude mapped to the
-// range 0-1 using the formula u = longitude / (2*PI) + 0.5, and v
-// will be the colatitude mapped to the range 0-1 using the
+// range 0-1 using the formula u = longitude / (2*PI) + 0.5, and
+// v will be the colatitude mapped to the range 0-1 using the
 // formula v = colatitude / PI.
 //
 template<typename T>
-static inline UV
+UV
 z_axis_spherical (const TVec<T> &vec)
 {
   return UV (clamp01 (z_axis_longitude (vec) * INV_PIf * 0.5f + 0.5f),
 	     clamp01 (z_axis_colatitude (vec) * INV_PIf));
 }
 
+
 // Return UV-encoded z-axis-based latitude-longitude coordinates
-// for VEC.  In the return value, u will be the longitude mapped to
-// the range 0-1 using the formula u = longitude / (2*PI) + 0.5,
-// and v will be the latitude mapped to the range 0-1 using the
-// formula v = latitude / PI + 0.5.
+// for VEC.
+//
+// In the return value, u will be the longitude mapped to the
+// range 0-1 using the formula u = longitude / (2*PI) + 0.5, and
+// v will be the latitude mapped to the range 0-1 using the
+// formula v = latitude / (PI/2) + 0.5.
 //
 template<typename T>
-static inline UV
+UV
 z_axis_latlong (const TVec<T> &vec)
 {
   return UV (clamp01 (z_axis_longitude (vec) * INV_PIf * 0.5f + 0.5f),
@@ -281,47 +301,57 @@ y_axis_latlong_to_vec (const UV &coords)
 // z-axis-based spherical/latlong vector construction functions.
 
 // Return a vector corresponding to the z-axis-based spherical
-// coordinates COLAT and LNG.  COLAT is the colatitude, defined as
-// the angle between the vector and the positive z-axis, with a
-// range of 0 - PI.  LNG is the longitude, defined as the angle
-// between a projection of the vector in the x-y plane and the
-// y-axis.
+// coordinates COS_COLAT and LNG.
+//
+// COS_COLAT is the _cosine_ of the colatitude (defined as the angle
+// between the vector and the positive z-axis), with a range of 0 - 1.
+// LNG is the longitude, defined as the angle between a projection of
+// the vector in the x-y plane and the y-axis.
 //
 template<typename T>
-static inline TVec<T>
-z_axis_spherical_to_vec (T colat, T lng)
-{
-  T sin_theta = sin (colat);
-  return TVec<T> (sin (lng) * sin_theta, cos (lng) * sin_theta, cos (colat));
-}
-
-// Return a vector corresponding to the z-axis-based spherical
-// coordinates COS_COLAT and LNG.  COS_COLAT is the _cosine_ of the
-// colatitude (defined as the angle between the vector and the
-// positive z-axis), with a range of 0 - 1.  LNG is the longitude,
-// defined as the angle between a projection of the vector in the
-// x-y plane and the y-axis.
-//
-template<typename T>
-static inline TVec<T>
+Vec
 z_axis_cos_spherical_to_vec (T cos_colat, T lng)
 {
-  T sin_theta = sqrt (1 - cos_colat * cos_colat);
-  return TVec<T> (sin (lng) * sin_theta, cos (lng) * sin_theta, cos_colat);
+  T sin_lat = cos_colat;
+  T cos_lat = sqrt (1 - sin_lat * sin_lat);
+  T cos_lng = cos (lng);
+  T sin_lng = sqrt (1 - cos_lng * cos_lng);
+  if (lng < 0 || lng > T (PI))
+    sin_lng = -sin_lng;		// sqrt formula returns abs value
+  return Vec (dist_t (cos_lng * cos_lat),
+	      dist_t (-sin_lng * cos_lat),
+	      dist_t (sin_lat));
 }
 
 // Return a vector corresponding to the z-axis-based spherical
-// coordinates LAT and LNG.  LAT is the latitude, defined as the
-// angle between the vector and the x-y plane, with a range of -PI/2
-// - PI/2.  LNG is the longitude, defined as the angle between a
-// projection of the vector in the x-y plane and the y-axis.
+// coordinates COLAT and LNG.
+//
+// COLAT is the colatitude, defined as the angle between the vector
+// and the positive z-axis, with a range of 0 - PI.  LNG is the
+// longitude, defined as the angle between a projection of the vector
+// in the x-y plane and the y-axis.
 //
 template<typename T>
-static inline TVec<T>
+inline Vec
+z_axis_spherical_to_vec (T colat, T lng)
+{
+  return z_axis_cos_spherical_to_vec (cos (colat), lng);
+}
+
+// Return a vector corresponding to the z-axis-based spherical
+// coordinates LAT and LNG.
+//
+// LAT is the latitude, defined as the angle between the vector and
+// the x-y plane, with a range of -PI/2 - PI/2.  LNG is the longitude,
+// defined as the angle between a projection of the vector in the x-y
+// plane and the y-axis.
+//
+template<typename T>
+inline Vec
 z_axis_latlong_to_vec (T lat, T lng)
 {
-  T cos_lat = cos (lat);
-  return TVec<T> (sin (lng) * cos_lat, cos (lng) * cos_lat, sin (lat));
+  T colat = T (PI / 2) - lat;
+  return z_axis_spherical_to_vec (colat, lng);
 }
 
 // Return a vector corresponding to the UV-encoded z-axis-based
@@ -331,11 +361,11 @@ z_axis_latlong_to_vec (T lat, T lng)
 // formula u = longitude / (2*PI) + 0.5, and v is the colatitude
 // mapped to the range 0-1 using the formula v = colatitude / PI.
 //
-static inline Vec
+inline Vec
 z_axis_spherical_to_vec (const UV &coords)
 {
-  dist_t lng = (coords.u - 0.5f) * PIf * 2;
-  dist_t colat = coords.v * PIf;
+  float lng = (coords.u - 0.5f) * PIf * 2;
+  float colat = coords.v * PIf;
   return z_axis_spherical_to_vec (colat, lng);
 }
 
@@ -343,14 +373,14 @@ z_axis_spherical_to_vec (const UV &coords)
 // latitude-longitude in COORDS.
 //
 // In COORDS, u is the longitude mapped to the range 0-1 using the
-// formula u = longitude / (2*PI) + 0.5, and v is the latitude
-// mapped to the range 0-1 using the formula v = latitude / PI + 0.5.
+// formula u = longitude / (2*PI) + 0.5, and v is the latitude mapped
+// to the range 0-1 using the formula v = latitude / (PI/2) + 0.5.
 //
-static inline Vec
+inline Vec
 z_axis_latlong_to_vec (const UV &coords)
 {
-  dist_t lng = (coords.u - 0.5f) * PIf * 2;
-  dist_t lat = (coords.v - 0.5f) * PIf;
+  float lng = (coords.u - 0.5f) * PIf * 2;
+  float lat = (coords.v - 0.5f) * PIf;
   return z_axis_latlong_to_vec (lat, lng);
 }
 
