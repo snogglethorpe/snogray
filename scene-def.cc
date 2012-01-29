7e141f9dc0b26fab6ec6ee8edbc7896309c01a4c
Miles Bader <miles@gnu.org>
2012-01-29 11:36:39 +0900

    Add -B/--background-orientation scene option
    This allows an environment-map to be rotated, oriented around another
    axis, or reversed.

--- scene-def.cc
+++ scene-def.cc
@@ -10,9 +10,12 @@
 // Written by Miles Bader <miles@gnu.org>
 //
 
+#include <cstdlib>
+
 #include "scene.h"
 #include "envmap.h"
 #include "envmap-light.h"
+#include "xform.h"
 #include "load-envmap.h"
 #include "far-light.h"
 #include "excepts.h"
@@ -84,18 +87,71 @@ SceneDef::load (Scene &scene, Camera &camera)
 	  Ref<Envmap> envmap
 	    = load_envmap (bg_spec, (fmt == "envmap") ? "" : fmt);
 
+	  // A frame which defines a transformation from
+	  // environment-map-coordinates to world coordinates.
+	  //
+	  Frame envmap_mapping_frame;
+
+	  // Environment-map orientation parameters.
+	  //
 	  // We use left-handed coordinates by default, and most
-	  // scenes have a verticle Y axis; most environment maps are
+	  // scenes have a vertical Y axis; most environment maps are
 	  // right-handed, and the environment-mapping code assumes a
-	  // verticle Z-axis.  So make a frame which maps accordingly.
+	  // vertical Z-axis.  The default rotation is chosen to
+	  // preserve compatibility with old scenes.
 	  //
-	  // The exact mapping is chosen to preserve compatibility
-	  // with old scenes.
+	  char axis = 'y', handedness = 'r';
+	  float rotation = 90;
+
+	  // If the user specified some non-default options for the
+	  // environment-map orientation, parse them.
 	  //
-	  // [This mapping should be a user option to adjust for
-	  // different environment maps. XXX]
+	  string bg_orient = params.get_string ("scene.background-orientation");
+	  if (! bg_orient.empty ())
+	    {
+	      bg_orient = downcase (bg_orient);
+
+	      const char *tail = bg_orient.c_str ();
+	      if (*tail == 'x' || *tail == 'y' || *tail == 'z')
+		axis = *tail++;
+	      if (*tail == 'r' || *tail == 'l')
+		handedness = *tail++;
+	      if (*tail)
+		{
+		  // Read rotation amount; note that we _add_ to the
+		  // default, which is non-zero.
+
+		  char *end;
+		  rotation += strtof (tail, &end);
+		  if (end == tail)
+		    throw bad_format ("Invalid background-orientation \""
+				      + bg_orient + "\"");
+		}
+	    }
+
+	  Xform xform;
+ 
+	  // If the environment-map "handedness" isn't our default
+	  // left-handedness, flip the transform, which will reverse
+	  // handedness.
 	  //
-	  Frame envmap_mapping_frame (Vec(0,0,1), Vec(-1,0,0), Vec(0,1,0));
+	  if (handedness == 'r')
+	    xform.scale (-1, 1, 1);
+
+	  // Do any desired about the vertical axis.
+	  //
+	  if (rotation != 0)
+	    xform.rotate_z (rotation * PIf / 180);
+
+	  // If the desired vertical axis isn't the default z-axis,
+	  // rotate the desired axis into z's place.
+	  //
+	  if (axis == 'x')
+	    xform.rotate_y (-PIf / 2);
+	  else if (axis == 'y')
+	    xform.rotate_x (-PIf / 2);
+
+	  envmap_mapping_frame.transform (xform);
 
 	  bg_light = new EnvmapLight (envmap, envmap_mapping_frame);
 	}
--- scene-def.h
+++ scene-def.h
@@ -1,6 +1,6 @@
 // scene-def.h -- Scene definition object
 //
-//  Copyright (C) 2005, 2006, 2007, 2008, 2010, 2011  Miles Bader <miles@gnu.org>
+//  Copyright (C) 2005-2008, 2010-2012  Miles Bader <miles@gnu.org>
 //
 // This source code is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
@@ -22,24 +22,35 @@
   -b, --background=BG        Use BG as a background and light-source;\n\
                                BG may be a color or the name of an\n\
                                environment-map image file\n\
-\
+  -B, --background-orientation=[AXIS][HAND][ROTATION]\n\
+                             Rotate the background and/or change its axis:\n\
+                               AXIS is the vertical axis, with values\n\
+                                  \"x\", \"y\", or \"z\" (default \"y\")\n\
+                               HAND is the \"handedness\", with values\n\
+                                  \"r\" or \"l\" (default \"r\")\n\
+                               ROTATION is an amount to rotate the background\n\
+                                 around the vertical axis, in degrees\n\
+\n\
   -I, --scene-options=OPTS   Set scene options; OPTS has the format\n\
                                OPT1=VAL1[,...]; current options include:\n\
                                  \"format\"    -- scene file type\n\
                                  \"background\"-- scene background"
                                
 //
-#define SCENE_DEF_SHORT_OPTIONS		"b:A:l:I:c:"
+#define SCENE_DEF_SHORT_OPTIONS		"b:B:I:"
 //
 #define SCENE_DEF_LONG_OPTIONS						\
     { "background",     required_argument, 0, 'b' },			\
-    { "scene-options", 	required_argument, 0, 'I' },			\
+    { "background-orientation",     required_argument, 0, 'B' },	\
+    { "scene-options", 	required_argument, 0, 'I' },
 
 #define SCENE_DEF_OPTION_CASES(clp, scene_def)				\
   case 'b':								\
     scene_def.params.set ("scene.background", clp.opt_arg ());		\
     break;								\
-									\
+  case 'B':								\
+    scene_def.params.set ("scene.background-orientation", clp.opt_arg ()); \
+    break;								\
   case 'I':								\
     clp.parse_opt_arg (scene_def.params);				\
     break;
