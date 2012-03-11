e62264d98115373ea719b72d98ea3ca4bace403c
Miles Bader <miles@gnu.org>
2012-03-12 00:22:01 +0900

    Return "double" from Rusage time methods
    Double has enough bits to store any time we want, and is a nice usable
    type, so there's really no point in using Timeval.

--- rusage.h
+++ rusage.h
@@ -1,6 +1,6 @@
 // rusage.h -- Resource usage measurement
 //
-//  Copyright (C) 2005, 2007, 2011  Miles Bader <miles@gnu.org>
+//  Copyright (C) 2005, 2007, 2011, 2012  Miles Bader <miles@gnu.org>
 //
 // This source code is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
@@ -15,20 +15,25 @@
 
 #include <sys/resource.h>
 
-#include "timeval.h"
 
 namespace snogray {
+
 
 struct Rusage : rusage
 {
   Rusage () { getrusage (RUSAGE_SELF, static_cast<struct rusage *>(this)); }
 
-  Timeval utime () const { return Timeval (ru_utime); }
-  Timeval stime () const { return Timeval (ru_stime); }
+  double utime () const
+  {
+    return double (ru_utime.tv_sec) + double (ru_utime.tv_usec) / 1e6;
+  }
+  double stime () const
+  {
+    return double (ru_stime.tv_sec) + double (ru_stime.tv_usec) / 1e6;
+  }
 };
+
 
 }
 
-#endif /* SNOGRAY_RUSAGE_H */
-
-// arch-tag: 7a4422b7-3908-44f2-9105-95b6905b6905
+#endif // SNOGRAY_RUSAGE_H
