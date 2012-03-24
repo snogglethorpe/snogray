17172752079d715e0e37f8890f8df43aba29b50b
Miles Bader <miles@gnu.org>
2012-03-24 11:11:07 +0900

    Move Lua scene handling into "snogray.scene" module

---
+++ scene.lua
@@ -0,0 +1,31 @@
+-- scene.lua -- Lua scene handling for snogray
+--
+--  Copyright (C) 2012  Miles Bader <miles@gnu.org>
+--
+-- This source code is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License as
+-- published by the Free Software Foundation; either version 3, or (at
+-- your option) any later version.  See the file COPYING for more details.
+--
+-- Written by Miles Bader <miles@gnu.org>
+--
+
+-- module
+--
+local scene = {}
+
+-- make the module callable, as a shortcut for "scene.new"
+--
+setmetatable (scene, { __call = function (m, ...) return m.new (...) end })
+
+-- imports
+--
+local raw = require "snogray.snograw"
+
+
+scene.new = raw.Scene
+
+
+-- return module
+--
+return scene
