74d48342295188e0111a515dbbddaeee6b22b736
Miles Bader <miles@gnu.org>
2012-04-14 09:15:41 +0900

    image-png.h: Omit directory from include of <png.h>
    We always use an appropriate search-path to find <png.h> (and the
    parent directory isn't necessarily in the search-path).

--- image-png.h
+++ image-png.h
@@ -1,6 +1,6 @@
 // image-png.h -- PNG format image handling
 //
-//  Copyright (C) 2005, 2006, 2007, 2011  Miles Bader <miles@gnu.org>
+//  Copyright (C) 2005-2007, 2011, 2012  Miles Bader <miles@gnu.org>
 //
 // This source code is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
@@ -15,7 +15,7 @@
 
 #include <cstdio>
 
-#include <libpng/png.h>
+#include <png.h>
 
 #include "image-byte-vec.h"
 
