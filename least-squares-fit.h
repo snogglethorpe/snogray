7f13c43ef5196a6bb51954b5913964f257aac68f
Miles Bader <miles@gnu.org>
2012-04-22 16:00:09 +0900

    Remove single-column-Y restriction from least_squares_fit

--- least-squares-fit.h
+++ least-squares-fit.h
@@ -21,28 +21,32 @@
 namespace snogray {
 
 
-// Return a least-squares fit, B, for the formula X * B ~= y.
+// Return a least-squares fit, B, for the formula X * B ~= Y.
 //
 // A least-squares fit minimizes the sum of the squares of the
-// differences between elements of X * B and y.
+// differences between elements of X * B and Y.
 //
-// y should be a single-column matrix with the same number of rows as
-// X, containing the values on the curve being fitted, y0...yn.  X
+// Y should be a matrix with the same number of rows as X, where each
+// column contains values y0...yn on a curve being fitted (if y has
+// multiple columns, each will be handled independently).  X
 // represents the set of functions being used to fit the curve, and
 // can have as many columns as there are functions.  The answer B will
-// be a single-column matrix with as many rows as X has columns, and
-// represents the set of scale factors, B0...Bm such that:
+// be a matrix with as many rows as X has columns, and as many columns
+// as Y.  Each column of B represents a set of scale factors, b0...bm
+// such that:
 //
-//   B0*[X0,0, ... X0,n] + ... + Bm-1 * [Xm-1,0, ... Xm-1,n] ~= [y0, ... yn]
+//   b0*[X0,0, ... X0,n] + ... + bm-1 * [Xm-1,0, ... Xm-1,n] ~= [y0, ... yn]
+//
+// where b and y are corresponding single columns of B and Y.
 //
 // This function only works the matrix X^T * X has full rank, that is,
 // all columns/rows of X^T * X are linearly independent.
 //
 template<typename T>
 Matrix<T>
-least_squares_fit (const Matrix<T> &X, const Matrix<T> &y)
+least_squares_fit (const Matrix<T> &X, const Matrix<T> &Y)
 {
-  ASSERT (X.rows() == y.rows());
+  ASSERT (X.rows() == Y.rows());
 
   // Transpose of X, X^T
   //
@@ -58,14 +62,14 @@ least_squares_fit (const Matrix<T> &X, const Matrix<T> &y)
   Matrix<T> R = Rt.transpose ();
 
   // As R^T is a lower-triangular matrix, we can use a forward-
-  // substitution step to solve the equation R^T * z = X^T * y, for z.
+  // substitution step to solve the equation R^T * Z = X^T * Y, for z.
   //
-  Matrix<T> z = forward_substitution (Rt, Xt * y);
+  Matrix<T> Z = forward_substitution (Rt, Xt * Y);
 
   // Similarly, as R is an upper-triangular matrix, we can use a
-  // back-substitution step to solve the equation R * B = z, for B.
+  // back-substitution step to solve the equation R * B = Z, for B.
   //
-  Matrix<T> B = back_substitution (R, z);
+  Matrix<T> B = back_substitution (R, Z);
 
   // And B is the answer!
   //
