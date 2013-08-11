4f8acc77f9cc4501e52489e90971c69fa81537d3
Miles Bader <miles@gnu.org>
2013-08-11 17:58:16 +0900

    config.mk: Add header comment

--- config.mk
+++ config.mk
@@ -1,3 +1,18 @@
+# Makefile fragment for Snogray global make variable definitions
+#
+# Automake Makefile template for snogray
+#
+#  Copyright (C) 2005-2013  Miles Bader <miles@gnu.org>
+#
+# This source code is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or (at
+# your option) any later version.  See the file COPYING for more details.
+#
+# Written by Miles Bader <miles@gnu.org>
+#
+
+
 ################################################################
 #
 # General compilation flags.  "OPT" and "DEBUG" intentionally have
