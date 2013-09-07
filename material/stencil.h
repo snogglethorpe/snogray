d8edd7c61fc2c56d1a74843bcd369481f1205fb2
Miles Bader <miles@gnu.org>
2013-09-07 14:18:36 +0900

    Set OCCLUSION_REQUIRES_TEX_COORDS flag in Stencil
    There's no effect yet, but it will be necessary in the future.

--- material/stencil.h
+++ material/stencil.h
@@ -29,7 +29,7 @@ public:
 
   Stencil (const TexVal<Color> &_opacity,
 	   const Ref<const Material> &_underlying_material)
-    : Material (PARTIALLY_OCCLUDING),
+    : Material (PARTIALLY_OCCLUDING|OCCLUSION_REQUIRES_TEX_COORDS),
       opacity (_opacity), underlying_material (_underlying_material)
   {
     bump_map = _underlying_material->bump_map;
