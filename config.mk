e02b945baa5837ceb00330713cae9d128dd49ed7
Miles Bader <miles@gnu.org>
2014-09-04 13:19:07 +0900

    Use -pthread for configuration too
    This is necessary for some thread-related configuration tests to do
    the right thing in some cases (and in general it's desirable for
    configuration to use an environment similar the actual build).

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@
 #
 # Automake Makefile template for snogray
 #
-#  Copyright (C) 2005-2013  Miles Bader <miles@gnu.org>
+#  Copyright (C) 2005-2014  Miles Bader <miles@gnu.org>
 #
 # This source code is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -27,7 +27,8 @@ DEBUG = -g
 # Set the default compiler options from OPT and DEBUG.  If the user
 # wants to change the optimization or debug settings, he should set
 # one of those on the make command line.  CXX_STD_FLAGS,
-# CXX_CHECK_FLAGS, and EXTRA_COMPILE_FLAGS are set by configure.
+# CXX_CHECK_FLAGS, CXX_THREAD_FLAGS, and EXTRA_COMPILE_FLAGS are set
+# by configure.
 #
 # [Note that using CXX_CHECK_FLAGS in AM_CFLAGS, which is used by the
 # C compiler, is a little hacky, but usually works in practice as the
@@ -36,14 +37,14 @@ DEBUG = -g
 # compiler, a separate "C_CHECK_FLAGS" variable could be introduced
 # which contains check options specifically for the C compiler.]
 #
-AM_CFLAGS = $(OPT) $(DEBUG) $(CXX_CHECK_FLAGS) $(EXTRA_COMPILE_FLAGS)
-AM_CXXFLAGS = $(OPT) $(DEBUG) $(CXX_STD_FLAGS) $(CXX_CHECK_FLAGS) $(EXTRA_COMPILE_FLAGS)
+AM_CFLAGS = $(OPT) $(DEBUG) $(CXX_THREAD_FLAGS) $(CXX_CHECK_FLAGS) $(EXTRA_COMPILE_FLAGS)
+AM_CXXFLAGS = $(OPT) $(DEBUG) $(CXX_STD_FLAGS) $(CXX_THREAD_FLAGS) $(CXX_CHECK_FLAGS) $(EXTRA_COMPILE_FLAGS)
 
 # Versions of AM_CFLAGS and AM_CXXFLAGS for situations where we want to
 # optimize for size.
 #
-AM_CFLAGS_SIZE = $(OPT_SIZE) $(DEBUG) $(EXTRA_COMPILE_FLAGS)
-AM_CXXFLAGS_SIZE = $(OPT_SIZE) $(DEBUG) $(CXX_STD_FLAGS) $(CXX_CHECK_FLAGS) $(EXTRA_COMPILE_FLAGS)
+AM_CFLAGS_SIZE = $(OPT_SIZE) $(DEBUG) $(CXX_THREAD_FLAGS) $(EXTRA_COMPILE_FLAGS)
+AM_CXXFLAGS_SIZE = $(OPT_SIZE) $(DEBUG) $(CXX_STD_FLAGS) $(CXX_THREAD_FLAGS) $(CXX_CHECK_FLAGS) $(EXTRA_COMPILE_FLAGS)
 
 # These options are required for correctness, and _must_ come _after_
 # any optimization options (gcc isn't very bright about parsing
